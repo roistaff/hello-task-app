05ccd27101ea7a670e52a4e3a7ba1e307f63924f
roistaff <118814428+roistaff@users.noreply.github.com>
2023-03-02 19:53:23 +0900

    Add files via upload

---
+++ app/app/myapp.py
@@ -0,0 +1,2 @@
+def hello_funck():
+	print("Hello,World")
---
+++ app/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='myapp',
+    version='0.1',
+    py_modules=['myapp'],
+    entry_points={
+        'console_scripts': [
+            'hello = myapp:hello_func',
+        ],
+    },
+)
