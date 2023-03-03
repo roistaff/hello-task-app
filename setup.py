93ba47fbb66743da97f9c1037d2fc2638be1cfc6
roistaff <118814428+roistaff@users.noreply.github.com>
2023-03-03 12:37:08 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hello-task',
+    version='0.2',
+    packages=['task_app'],
+    description='task app',
+    author='Roi Staff',
+    author_email='roistaff1983@gmail.com',
+    entry_points={
+        'console_scripts': [
+            'hello-task = task_app.task_app.py:autostart',
+        ],
+    },
+)
---
+++ task_app/task_app.py
@@ -0,0 +1,59 @@
+import csv
+import sys
+import datetime
+import os
+args = sys.argv
+def task_read():
+    with open("tasks/tasks.csv","r") as f:
+        reader = csv.reader(f)
+        tasks = [row for row in reader]
+        return tasks
+def task_add(to_do,time):
+    try:
+        time = datetime.datetime.strptime(time,'%Y-%m-%d')
+    except:
+        print("format error. format is 'Y-m-d'")
+        sys.exit()
+    with open("tasks/tasks.csv","a") as f:
+        writer = csv.writer(f)
+        writer.writerow([to_do,time])
+def task_remove(delete_row):
+    if delete_row > len(task_read()):
+        print("Error.No task.")
+    else:
+        delete_row = delete_row - 1
+        with open('tasks/tasks.csv', 'r') as f:
+            reader = csv.reader(f)
+            rows = []
+            for i, row in enumerate(reader):
+                if i != delete_row:
+                    rows.append(row)
+        with open('tasks/tasks.csv', 'w') as f:
+            writer = csv.writer(f)
+            for row in rows:
+                writer.writerow(row)
+        print("remove.")
+def task_cal():
+    tasks = task_read()
+    dt_now = datetime.datetime.now()
+
+def default_start():
+    if os.path.exists("tasks"):
+        pass
+    else:
+        os.mkdir("tasks")
+        with open("tasks/tasks.csv","w") as f:
+            pass
+def autostart():
+    default_start()
+    if len(args) == 1:
+        print("Hello")
+    else:
+        if args[1] == "-add":
+            task_add(args[2],args[3])
+        elif args[1] == "-show":
+            tasks = task_read()
+            for t in tasks:
+                print("Do:",t[0],"When:",t[1])
+        elif args[1] == "-remove":
+            task_remove(int(args[2]))
